88a738c50a187e38de8b370a01b504a6b6b8ec16
Vit Benton <viteksk8alot@gmail.com>
2020-04-19 13:35:44 -0400

    Init

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ module.exports = {
     connect: () => {
         // Connect to the Mongo DB
         // =============================================================
-        const DB_NAME = "analog"
+        const DB_NAME = "portfolio_2020"
         const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`
         // Set mongoose to leverage built in JavaScript ES6 Promises
         mongoose.Promise = Promise
