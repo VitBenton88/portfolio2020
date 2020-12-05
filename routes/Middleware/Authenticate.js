be9ec1c088ad1ad00eba1519c7bad396c8a5aa40
Vit Benton <viteksk8alot@gmail.com>
2020-12-04 22:50:51 -0500

    Added API security

--- routes/Middleware/Authenticate.js
+++ routes/Middleware/Authenticate.js
@@ -3,5 +3,18 @@ module.exports = (app, ensureAuthenticated) => {
   // Authenticate on all admin routes
   // =============================================================
   app.use('/admin/*', ensureAuthenticated)
+  
+  // Authenticate on all api routes
+  // =============================================================
+  app.use('/api/*', (req, res, next) => {
+    const { originalUrl } = req
+    const safe_paths = ['/api/password']
+      
+    if (!req.isAuthenticated() && !safe_paths.includes(originalUrl) ) {
+        return req.sendStatus(403)
+    }
+    
+    next()
+  })
 
 }
